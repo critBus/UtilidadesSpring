b1c22bf9bfe0585994b5cc6b41346948e86ea972
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2024-08-27 12:14:16 -0400

    cambios desconocidos

--- UtilidadesSpring/Program.cs
+++ UtilidadesSpring/Program.cs
@@ -12,21 +12,29 @@ namespace UtilidadesSpring
         static void Main(string[] args)
         {
             generalClases();
+            
+            //cwl("algo");
             endC();
         }
 
         public static void generalClases() {
             ConfiguracionGeneral cnf = new ConfiguracionGeneral(
                 "cu.edu.unah.demo"
-                , @"D:\_Cosas\pincha\Isbel\Spring Security RestApi - Spring Boot Proyect modificar\RestApiCapasitacionUniversitaria\src\main\java\cu\edu\unah\demo"
+                , @"D:\TRABAJO\NotasEstudiantes\experimentos\automatico"
                 , "Rest"
-                , @"D:\_Cosas\pincha\Isbel\CapasitacionUniversitariaWeb_JAVA_8\src\java\Rest"
+                , @"D:\TRABAJO\NotasEstudiantes\experimentos\automatico"
                 );
-            //new CreadorClasesRepositorio(cnf).crearClasesRepository();
-            //new CreadorClasesServicios(cnf).crearClasesServicios();
-            //new CreadorClasesControles(cnf).crearClasesControlador();
-            new CreadorClasesRestJava8(cnf).crearClasesRest();
-            cwl("termino");
+            //ConfiguracionGeneral cnf = new ConfiguracionGeneral(
+            //    "cu.edu.unah.demo"
+            //    , @"D:\_Cosas\pincha\Isbel\Spring Security RestApi - Spring Boot Proyect modificar\RestApiCapasitacionUniversitaria\src\main\java\cu\edu\unah\demo"
+            //    , "Rest"
+            //    , @"D:\_Cosas\pincha\Isbel\CapasitacionUniversitariaWeb_JAVA_8\src\java\Rest"
+            //    );
+            new CreadorClasesRepositorio(cnf).crearClasesRepository();
+            new CreadorClasesServicios(cnf).crearClasesServicios();
+            new CreadorClasesControles(cnf).crearClasesControlador();
+            //new CreadorClasesRestJava8(cnf).crearClasesRest();
+            cwl("termino2");
         }
     }
 }
